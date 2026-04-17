a5f1f480848821f4ae218326e961f5e266858a1c
Cedric <crubbertire@rogers.com>
2026-04-17 15:25:51 -0700

    Username and Initial conversions

--- Main.java
+++ Main.java
@@ -6,6 +6,22 @@ public class Main {
       String firstName = sc.nextLine().trim();
       String lastName = sc.nextLine().trim();
 
+      String username = firstName + lastName;
 
+      // 2 options:
+
+      // Option 1:
+      // Get chars from name,
+      // convert char array to string
+      // char[] initialChars = {firstName.charAt(0), lastName.charAt(0)};
+      // String initials = new String(initialChars);
+
+      // Option 2:
+      // Grab the substrings of the first letters.
+      String initials = firstName.substring(0, 1) + lastName.substring(0, 1);
+      
+
+      System.out.println("Username: " + username);
+      System.out.println("Initials: " + initials);
   }
 }
